3008ce1692de0419d8e7fedc0acbe47d2aef2937
Leo Batyuk <leonid.batyuk@wooga.net>
2021-11-09 20:51:56 +0100

    Finalize text footprint

--- src/footprints/text.js
+++ src/footprints/text.js
@@ -4,9 +4,15 @@ module.exports = {
         text: '',
         h_size: 1,
         v_size: 1,
-        thickness: 0.15
+        thickness: 0.15,
+        justify: null
     },
-    body: p => `
-        (gr_text "${p.text}" ${p.at} (layer ${p.param.layer}) (effects (font (size ${p.param.h_size} ${p.param.v_size}) (thickness ${p.param.thickness}))))
-    `
+    body: p => {
+        justify = p.param.justify && `(justify ${p.param.justify})` || '';
+        return `
+            (gr_text "${p.param.text}" ${p.at} (layer ${p.param.layer})
+                (effects (font (size ${p.param.h_size} ${p.param.v_size}) (thickness ${p.param.thickness})) ${justify})
+            )
+        `
+    }
 }
