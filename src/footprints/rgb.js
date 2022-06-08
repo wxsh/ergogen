3a67ea3e335c6f099dd33d92be07622f88d611ea
Andreas Karlsvik <andreas@2park.no>
2022-06-08 09:30:18 +0200

    mirror rgb

--- src/footprints/rgb.js
+++ src/footprints/rgb.js
@@ -7,17 +7,19 @@ module.exports = {
     },
     params: {
         class: 'LED',
-        side: 'F',
+        side: 'F'
     },
     body: p => {
-    function standard(font_effect) { 
+        function standard(def_pos) {
         return `
+    
         (module WS2812B (layer F.Cu) (tedit 53BEE615)
+
             ${p.at /* parametric position */}
 
             ${'' /* footprint reference */}
             (fp_text reference "${p.ref}" (at 0 0) (layer ${p.param.side}.SilkS) ${p.ref_hide} (effects (font (size 1.27 1.27) (thickness 0.15))))
-            (fp_text value "" (at 0 0) (layer ${p.param.side}.SilkS) hide (effects (font (size 1.27 1.27) (thickness 0.15)})))
+            (fp_text value "" (at 0 0) (layer ${p.param.side}.SilkS) hide (effects (font (size 1.27 1.27) (thickness 0.15))))
 
             (fp_line (start -1.75 -1.75) (end -1.75 1.75) (layer ${p.param.side}.SilkS) (width 0.15))
             (fp_line (start -1.75 1.75) (end 1.75 1.75) (layer ${p.param.side}.SilkS) (width 0.15))
@@ -29,9 +31,8 @@ module.exports = {
             (fp_line (start 2.5 2.5) (end 2.5 -2.5) (layer ${p.param.side}.SilkS) (width 0.15))
             (fp_line (start 2.5 -2.5) (end -2.5 -2.5) (layer ${p.param.side}.SilkS) (width 0.15))
 
-            (fp_poly (pts (xy 4 2.2) (xy 4 0.375) (xy 5 1.2875)) (layer ${p.param.side}.SilkS) (width 0.1))
-            `
-        }
+            (fp_poly (pts (xy ${def_pos}4 ${def_pos}2.2) (xy ${def_pos}4 ${def_pos}0.375) (xy ${def_pos}5 ${def_pos}1.2875)) (layer ${p.param.side}.SilkS) (width 0.1))
+            `}
         
         function pins(def_neg, def_pos) {
             return `
@@ -44,19 +45,18 @@ module.exports = {
             (pad 22 smd rect (at ${def_neg}2.5 ${def_pos}1.6 ${p.rot}) (size 2 1.2) (layers ${p.param.side}.Cu ${p.param.side}.Paste ${p.param.side}.Mask) ${p.net.dout.str})
             (pad 33 smd rect (at ${def_pos}2.5 ${def_pos}1.6 ${p.rot}) (size 2 1.2) (layers ${p.param.side}.Cu ${p.param.side}.Paste ${p.param.side}.Mask) ${p.net.GND.str})
             (pad 44 smd rect (at ${def_pos}2.5 ${def_neg}1.6 ${p.rot}) (size 2 1.2) (layers ${p.param.side}.Cu ${p.param.side}.Paste ${p.param.side}.Mask) ${p.net.din.str})
-            `
-        }
-
-        if(p.param.side == 'F') {
-            return `
-              ${standard()}
-              ${pins('-', '')}
-              `
-          } else {
-            return `
-              ${standard('(justify mirror)')}
-              ${pins('', '-')}
-              `
-          }
+            )
+            `}
+    if (p.param.side == 'F') {
+    return `
+        ${standard('')}
+        ${pins('-', '')}
+    `
+    } else {
+        return `
+        ${standard('-')}
+        ${pins('', '-')}
+    `
+    }
     }
 }
