ab1f5207d379a65cccdb10ac82f0eaeb1be18825
Andreas Karlsvik <andreas@2park.no>
2022-06-09 09:21:41 +0200

    parametric traces

--- src/footprints/tracefix.js
+++ src/footprints/tracefix.js
@@ -1,16 +1,21 @@
 module.exports = {
-    body: p => `
-    (segment (start 146.141827 34.476533) (end 149.673467 34.476533) (width 0.25) (layer F.Cu) (net 40))
-    (segment (start 152.528372 31.621628) (end 152.528372 29.300698) (width 0.25) (layer F.Cu) (net 40))
-    (segment (start 149.673467 34.476533) (end 152.528372 31.621628) (width 0.25) (layer F.Cu) (net 40))
+    params: {
+        variant: 'segment',
+        net: undefined,
+        side: 'F',
+        from: [undefined, undefined],
+        to: [undefined, undefined],
+    },
+    body: p => {
 
-    (segment (start 142.234825 21.078667) (end 144.263492 19.05) (width 0.25) (layer F.Cu) (net 26))
-    (segment (start 144.263492 19.05) (end 150.01875 19.05) (width 0.25) (layer F.Cu) (net 26))
-    (segment (start 150.01875 19.05) (end 151.60625 19.05) (width 0.25) (layer F.Cu) (net 26))
-    (segment (start 151.60625 19.05) (end 153.19375 20.6375) (width 0.25) (layer F.Cu) (net 26))
-    (via (at 153.19375 23.8125) (size 0.8) (drill 0.4) (layers F.Cu B.Cu) (net 26))
-    (segment (start 153.19375 20.6375) (end 153.19375 23.8125) (width 0.25) (layer F.Cu) (net 26))
-    (segment (start 153.19375 25.878646) (end 152.598659 26.473737) (width 0.25) (layer B.Cu) (net 26))
-    (segment (start 153.19375 23.8125) (end 153.19375 25.878646) (width 0.25) (layer B.Cu) (net 26))
-    `
+    if (p.param.variant == 'segment') {
+        return `
+        (segment (start ${p.param.from[0]} ${p.param.from[1]}) (end ${p.param.to[0]} ${p.param.to[1]}) (width 0.25) (layer ${p.param.side}.Cu) (net ${p.param.net}))
+        `
+    } else if (p.param.variant == 'via') {
+        return `
+        (via (at ${p.param.from[0]} ${p.param.from[1]}) (size 0.8) (drill 0.4) (layers F.Cu B.Cu) (net ${p.param.net}))
+        `
+    }
+    }
 }
